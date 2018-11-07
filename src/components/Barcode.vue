c56e4abd2ece3d10a1b9cf755046c6216e20f1db
Zim <zimtsui@gmail.com>
2018-11-08 02:36:32 +0800

    developing

---
+++ src/components/Barcode.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div: canvas(ref="barcode")
+</template>
+
+<script>
+import JsBarcode from 'jsbarcode';
+
+export default {
+    props: [
+        'code',
+    ],
+    mounted() {
+        JsBarcode(this.$refs.barcode, this.code);
+    },
+};
+</script>
